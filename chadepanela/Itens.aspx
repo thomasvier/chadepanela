1dfae430610c2cbdae7b90f80f810e9e9db0a84b
thomasvier <thomasvier@gmail.com>
2015-08-17 14:43:31 -0300

    r

---
+++ chadepanela/Itens.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Itens.aspx.cs" Inherits="chadepanela.Itens" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div style="margin-left: 120px; margin-top: 20px">
+        <asp:GridView ID="gvItens" runat="server" AutoGenerateColumns="false" GridLines="None" Width="900" CssClass="mGrid"
+            ShowHeader="false"
+            OnPreRender="gvItens_PreRender"
+            OnRowCommand="gvItens_RowCommand">
+            <Columns>
+                <asp:BoundField DataField="id" SortExpression="id" HeaderStyle-CssClass="gridHidden" ItemStyle-CssClass="gridHidden" />
+                <asp:BoundField DataField="descricao" SortExpression="descricao" />
+                <asp:TemplateField>
+                    <ItemTemplate> 
+                        <div class="row" style="margin-left: 500px">
+                            <div class="col-lg-2">
+                                <asp:ImageButton ID="btnStatus" runat="server" CommandName="status" CommandArgument="<%# ((GridViewRow) Container).RowIndex %>" CssClass="btnIcon" ToolTip="Este item é meu :D" />
+                            </div>
+                            <div class="col-lg-8">                        
+                                <asp:Label ID="lblStatus" runat="server"></asp:Label>
+                            </div>
+                        </div>                   
+                    </ItemTemplate>
+                </asp:TemplateField>            
+            </Columns>
+        </asp:GridView>
+    </div>
+</asp:Content>
---
+++ chadepanela/Itens.aspx.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace chadepanela
+{
+    public partial class Itens : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!Page.IsPostBack)
+                bindarGrid();
+        }
+
+        private void bindarGrid()
+        {
+            cpEntities db = new cpEntities();
+
+            var itens = (from i in db.item
+                         orderby i.descricao
+                         select i).ToList();
+
+            gvItens.DataSource = itens;
+            gvItens.DataBind();
+        }
+
+        protected void gvItens_PreRender(object sender, EventArgs e)
+        {
+            cpEntities db = new cpEntities();
+
+            foreach(GridViewRow row in gvItens.Rows)
+            {
+                int codigo = int.TryParse(row.Cells[0].Text, out codigo) ? codigo : 0;
+
+                var stat = (from i in db.item
+                            where i.id.Equals(codigo)
+                            select i.status).FirstOrDefault();
+
+                if (stat.Equals("T"))
+                {
+                    ((ImageButton)row.Cells[2].FindControl("btnStatus")).ImageUrl = "~/Imagens/delete.png";
+                    ((ImageButton)row.Cells[2].FindControl("btnStatus")).Enabled = false;
+                    ((Label)row.Cells[2].FindControl("lblStatus")).Text = "Alguém já comprou este item.";
+                }
+                else
+                {
+                    ((ImageButton)row.Cells[2].FindControl("btnStatus")).ImageUrl = "~/Imagens/gift.png";
+                    ((ImageButton)row.Cells[2].FindControl("btnStatus")).Enabled = true;
+                    ((Label)row.Cells[2].FindControl("lblStatus")).Text = "Dê este item.";
+                }
+            }
+        }
+
+        protected void gvItens_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if(e.CommandName.Equals("status"))
+            {
+                cpEntities db = new cpEntities();
+
+                int index = int.TryParse(e.CommandArgument.ToString(), out index) ? index : 0;
+                int id = int.TryParse(gvItens.Rows[index].Cells[0].Text, out id) ? id : 0;
+
+                var edit = (from i in db.item
+                            where i.id.Equals(id)
+                            select i).FirstOrDefault();
+
+                if(edit.status.Equals("F"))
+                    edit.status = "T";
+
+
+                db.SaveChanges();
+
+                bindarGrid();
+            }
+        }
+    }
+}
